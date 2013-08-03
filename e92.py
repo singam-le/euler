8ccdf4b95b858e7aaf68405166cb946ab88b18f6
shanx <shanx@ubuntu.(none)>
2013-08-02 21:35:59 -0700

    changed to bits

--- e92.py
+++ e92.py
@@ -6,28 +6,32 @@ def multDigits(n):
         n=n/10
     return reduce (lambda x,y: (x)+(y*y), l,0)
 
-
-num = set()
-start = 85
-alloc = start * 10
-ar = array('B', 'a'*alloc)
-for i in xrange(0, alloc):
-    ar[i] &= 0
+start=99999
+alloc = 81*16*8
+print alloc
+ar = array('i', [0]*alloc)
 k=1
-def getIndex(val):
-    q,m = divmod(val,8)
-    print q, m
+
+def getItem(val):
+    q,m = divmod(val,16)
+    #print "in", q, val, alloc
+    if q >= alloc: print "bigger", q, alloc,val
+    return ar[q] & k<<m
+
+def setItem(val):
+    q,m = divmod(val,16)
     ar[q] = ar[q] | k<<m
 
-while(start):
-    print start
-    if not ar[start]:
-        getIndex(start)
-        ar[start] = start
-        start=multDigits(start)
-    else:
-        print "shanx .. done", start
-        start = 0
+for i in xrange(99,start,1):
+    start = i
+    while(start):
+        #print start
+        if not getItem(start):
+            setItem(start)
+            start=multDigits(start)
+        else:
+            if start==89: print i
+            break
 
 
 

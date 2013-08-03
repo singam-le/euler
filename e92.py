c448dc69c8ab6bccb72f49c8176b6733bb428df2
shanx <shanx@ubuntu.(none)>
2013-08-03 09:19:11 -0700

    modified for better alloc

--- e92.py
+++ e92.py
@@ -1,4 +1,5 @@
 from array import array
+# return sq. of all digits in num (n)
 def multDigits(n):
     l = []
     while (n):
@@ -6,34 +7,40 @@ def multDigits(n):
         n=n/10
     return reduce (lambda x,y: (x)+(y*y), l,0)
 
-start=99999
-alloc = 81*16*8
-print alloc
-ar = array('i', [0]*alloc)
-k=1
-
+# return 1 of index location if already occupied
+# location of bit array
 def getItem(val):
     q,m = divmod(val,16)
-    #print "in", q, val, alloc
-    if q >= alloc: print "bigger", q, alloc,val
-    return ar[q] & k<<m
+    return ar[q] & 1<<m
 
+# set the index of the location in the bit array 1
 def setItem(val):
     q,m = divmod(val,16)
-    ar[q] = ar[q] | k<<m
+    ar[q] = ar[q] | 1<<m
 
-for i in xrange(99,start,1):
-    start = i
-    while(start):
-        #print start
-        if not getItem(start):
-            setItem(start)
-            start=multDigits(start)
-        else:
-            if start==89: print i
-            break
+def e92Calc():
+    start=999999
+    print alloc
 
+    for i in xrange(85,start+1,1):
+        start = multDigits(i)
+        # initialize the bit array
+        ar = array('i', [0]*alloc)
+        while(start):
+            if not getItem(start):
+                setItem(start)
+                start=multDigits(start)
+            else:
+                if start==89: 
+                    print i
+                break    
 
-
+# here the 81 is sq. of max digit i.e 9
+# 6 is no. of digits in num
+# 16 is the size of int in array. to convert to bit array
+alloc = 81*6/16 + 1
+ar = array('i', [0]*alloc)
+if __name__ == '__main__':
+    e92Calc()
 
 
